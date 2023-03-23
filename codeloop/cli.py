2c036fff2bd12844befe0502778feda716d69a61
helloworld <s@thupukari.com>
2023-03-23 14:05:17 -0700

    Add template

--- codeloop/cli.py
+++ codeloop/cli.py
@@ -51,6 +51,7 @@ def generate(demo):
     package_info += f"\nRelative Path: [bold]{relative_path}[/bold]"
 
     panel = Panel(package_info, title="Package Information", border_style="blue")
+    console.print(panel)
 
     controller = ChatGPTController(package_name, requirements, relative_path)
     controller.get_commands_and_options()
